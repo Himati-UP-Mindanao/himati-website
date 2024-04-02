1946dcd5f609ccdf90b909701dabbe0d8eb629f9
devRaxx <rpcastaneros@up.edu.ph>
2024-04-03 03:08:56 +0800

    Added Seach Bar

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import HimatiIcon from "../../assets/himati-icon.svg";
+import { IoSearchSharp } from "react-icons/io5";
 const Header = () => {
   return (
     <header>
@@ -9,13 +10,21 @@ const Header = () => {
           <Image src={HimatiIcon} alt=""></Image>
           <h1>HIMATI</h1>
         </div>
-        <div className="flex flex-row gap-x-10">
+        <div className="flex flex-row items-center gap-x-10">
           <a href="/">HOME</a>
           <a href="/">NEWS</a>
           <a href="/">FEATURES</a>
           <a href="/">KULTURA</a>
           <a href="/">OPINION</a>
           <a href="/">ABOUT US</a>
+          <div className="flex items-center bg-white border-2 border-black rounded-full p-2">
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-transparent text-black font-bold placeholder-black focus:outline-none"
+            />
+            <IoSearchSharp />
+          </div>
         </div>
       </div>
     </header>
