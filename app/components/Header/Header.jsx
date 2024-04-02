d2d676c81626418c6c78d78a5edce04ccbc6b64a
devRaxx <rpcastaneros@up.edu.ph>
2024-04-03 02:49:16 +0800

    Added Intial Texts

--- app/components/Header/Header.jsx
+++ app/components/Header/Header.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import Image from "next/image";
+import HimatiIcon from "../../assets/himati-icon.svg";
 const Header = () => {
-  return <header>{/* Header Content */}</header>;
+  return (
+    <header>
+      <div className="flex flex-col items-center bg-red-400">
+        <div className="flex flex-row items-center">
+          <Image src={HimatiIcon} alt=""></Image>
+          <h1>HIMATI</h1>
+        </div>
+        <div className="flex flex-row gap-x-10">
+          <a href="/">HOME</a>
+          <a href="/">NEWS</a>
+          <a href="/">FEATURES</a>
+          <a href="/">KULTURA</a>
+          <a href="/">OPINION</a>
+          <a href="/">ABOUT US</a>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
